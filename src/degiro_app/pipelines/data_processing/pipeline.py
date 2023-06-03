f45b6163d6c7b828047ac3b7075c3c6df8d84b9d
Ignacio Moya <imoyared@gmail.com>
2023-06-03 15:39:12 +0200

    feat(stocks): added global return_df in an extra step

--- src/degiro_app/pipelines/data_processing/pipeline.py
+++ src/degiro_app/pipelines/data_processing/pipeline.py
@@ -8,8 +8,6 @@ from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import (
     node_dataset_clean,
     node_return_portfolio,
-    return_dividends,
-    return_portfolio,
 )
 
 
@@ -23,16 +21,10 @@ def create_pipeline(**kwargs) -> Pipeline:
                 name="split_description_node",
             ),
             node(
-                func=return_portfolio,
+                func=node_return_portfolio,
                 inputs=["degiro_app-account-clean"],
                 outputs="portfolio_summary",
                 name="return_on_portfolio_node",
-            ),
-            node(
-                func=return_dividends,
-                inputs=["degiro_app-account-clean"],
-                outputs="dividends_summary",
-                name="dividends_node",
             ),
         ]
     )
